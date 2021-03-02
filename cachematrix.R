69833179ed667cd3c4edb52bd91597827d03eded
abbywaiting <abbydubouzet@gmail.com>
2021-03-02 17:18:46 +0800

    my changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The 2 functions lessens the computation cost from matrix inversion
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Inverses cache through the use of matrix
+makeCacheMatrix <- function(mtrxOriginal = matrix()) {
+        # Set NULL value to mtrxInverse
+        mtrxInverse <- NULL
+        
+        set <- function(y) {
+          # Makes mrtxOriginal equal to input value
+          mtrxOriginal <<- y
+          # Clears value cached by cacheSolve
+          mtrxInverse <<- NULL
+        }
+        
+        # Gets value of mtrxOriginal
+        get <- function() mtrxOriginal
+        # Sets the inverse of mtrxInverse
+        setInverse <- function(mean) mtrxInverse <<- mean
+        # Gets the inverse of mtrxInverse
+        getInverse <- function() mtrxInverse
+        # Creates an object through a returned list
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Evaluates the inverse of makeCacheMatrix's returned value
+cacheSolve <- function(mtrxOriginal, ...) {
+        # Calls getInverse function from makeCacheMatrix
+        mtrxInverse <- mtrxOriginal$getInverse()
+        
+        # Checks if object mtrxInverse is not NULL
+        if(!is.null(mtrxInverse)) {
+          # If not then a message and the inverse will be returned to the parent environment
+          message("getting cached data")
+          return(mtrxInverse)
+        }
+        
+        # If the if-statement is false, the input object will be made an inverse
+        data <- mtrxOriginal$get()
+        mtrxInverse <- solve(data, ...)
+        mtrxOriginal$setInverse(mtrxInverse)
+        # Also, the object would be outputted
+        mtrxInverse
 }
